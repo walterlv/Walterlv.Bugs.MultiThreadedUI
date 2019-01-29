8b599983bc353be6e92e3146627aad0188217beb
walterlv <xblero@gmail.com>
2019-01-29 09:18:01 +0800

    Basic app with multiple window.

---
+++ Walterlv.Bugs.MultiThreadedUI/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Threading;
+
+namespace Walterlv.Bugs.MultiThreadedUI
+{
+    public class Program
+    {
+        [STAThread]
+        private static void Main(string[] args)
+        {
+            RunSplashWindow();
+            var app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+
+        private static void RunSplashWindow()
+        {
+            var thread = new Thread(() =>
+            {
+                SynchronizationContext.SetSynchronizationContext(
+                    new DispatcherSynchronizationContext(
+                        Dispatcher.CurrentDispatcher));
+
+                var window = new SplashWindow();
+                window.Show();
+
+                Dispatcher.Run();
+            })
+            {
+                IsBackground = true,
+            };
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
+        }
+    }
+}
